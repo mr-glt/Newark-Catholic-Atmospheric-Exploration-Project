10b87c3ee5d34784625255870305a96c8a3392f0
mr-glt <colson72098@gmail.com>
2015-05-27 21:50:54 -0400

    Added support for RTC and test code for getting date and time.

--- Sensors/Sensor-Main.ino
+++ Sensors/Sensor-Main.ino
@@ -1,7 +1,12 @@
-
+//Imports
 #include <SD.h>
 #include "Wire.h"
+
+//Definations
+#define DS1307_ADDRESS 0x86;//Need to get updated I2C Address
+byte zero = 0x00; //Needed as a workaround for issue #537 RTC Support
 File testFile;
+
 
 void setup(){
 	Serial.begin(9600);
@@ -30,4 +35,31 @@ void loop(){
     	Serial.println("error opening test.txt");
   	}
 
+  	//This is for testing the RTC
+  	Wire.beginTransmission(DS1307_ADDRESS);
+ 	Wire.write(zero); //Workaround	
+   	Wire.endTransmission();
+
+	Wire.requestFrom(DS1307_ADDRESS, 7);
+
+	int second = bcdToDec(Wire.read());
+  	int minute = bcdToDec(Wire.read());
+ 	int hour = bcdToDec(Wire.read() & 0b111111); //24 hour time
+ 	int weekDay = bcdToDec(Wire.read()); //0-6 -> sunday - Saturday
+ 	int monthDay = bcdToDec(Wire.read());
+  	int month = bcdToDec(Wire.read());
+  	int year = bcdToDec(Wire.read());
+
+  	//print the date EG   3/1/11 23:59:59
+  	Serial.print(month);
+  	Serial.print("/");
+  	Serial.print(monthDay);
+  	Serial.print("/");
+  	Serial.print(year);
+  	Serial.print(" ");
+  	Serial.print(hour);
+  	Serial.print(":");
+  	Serial.print(minute);
+  	Serial.print(":");
+  	Serial.print(second);
 }
